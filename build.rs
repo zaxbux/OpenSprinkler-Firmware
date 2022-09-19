20a5c98a76825bef8f6a1a606e9591dff37cf323
Zachary Schneider <hello@zacharyschneider.ca>
2022-09-19 16:33:10 -0700

    update build script

--- build.rs
+++ build.rs
@@ -1,14 +1,26 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::io::Write;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("user_agent_header.rs");
+    let dest_path = Path::new(&out_dir).join("build_constants.rs");
 
     let ua = env!("CARGO_PKG_VERSION");
+    
+    let mut w: Vec<u8> = Vec::new();
 
-    fs::write(&dest_path, format!("const USER_AGENT_STRING: &'static str = \"OpenSprinkler/{} (rust)\";", ua)).unwrap();
+    writeln!(w, "pub mod constants {{")?;
+    writeln!(w, "pub const USER_AGENT_STRING: &'static str = \"OpenSprinkler/{} (rust)\";", ua)?;
+    writeln!(w, "pub const MAX_EXT_BOARDS: usize = {};", option_env!("MAXIMUM_EXT_BOARDS").unwrap_or("24"))?;
+    writeln!(w, "}}")?;
+
+    fs::write(&dest_path, w)?;
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
+    println!("cargo:rerun-if-env-changed=MAXIMUM_EXT_BOARDS");
+
+    Ok(())
 }
